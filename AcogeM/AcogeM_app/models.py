1a6cd6a9a30e31d57a2615b2e34bea3de0478027
Israel Silva <escorpion45678@gmail.com>
2021-10-05 14:17:09 +0200

    Commit 5

--- AcogeM/AcogeM_app/models.py
+++ AcogeM/AcogeM_app/models.py
@@ -3,17 +3,31 @@ from django.db import models
 # Create your models here.
 
 class Ciudad(models.Model):
-    nombre = models.CharField('Nombre', max_length=50)
+    nom = models.CharField('Nombre', max_length=50)
+    pob = models.IntegerField('Población', default=0)
 
 class Protectora(models.Model):
     ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
-    nombre = models.CharField('Nombre', max_length=100)
-    direccion = models.CharField('Direccion', max_length=200)
+    nom = models.CharField('Nombre', max_length=100)
+    direc = models.CharField('Direccion', max_length=200)
 
 class Animal(models.Model):
+
+	PERRO = 1
+	GATO = 2
+	HURON = 3
+	CONEJO = 4
+
+	TIPOS_ANIMALES (
+		(PERRO, "Perro"),
+		(GATO, "Gato"),
+		(HURON, "Huron"),
+		(CONEJO, "Conejo"),
+	)
+
 	protectora = models.ForeignKey(Protectora, on_delete=models.CASCADE)
-	tipo = models.CharField(max_length=50)
-	nombre = models.CharField('Nombre', max_length=50)
-	descripcion = models.CharField('Descripción', max_length=200)
-	foto = models.ImageField('Imagen', blank=True, null=True, upload_to=localizacion_imagen)
+	tipo = models.PositiveSmallIntegerField('Tipo', choices=TIPOS_ANIMALES, default=PERRO)
+	nom = models.CharField('Nombre', max_length=50)
+	descrip = models.CharField('Descripción', max_length=200)
+#	foto = models.ImageField('Imagen', blank=True, null=True, upload_to=localizacion_imagen)
 
