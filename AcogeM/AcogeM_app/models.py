16e278464df7c95e6a1b42a4b828a8e5742098af
Israel Silva <escorpion45678@gmail.com>
2021-10-06 18:14:33 +0200

    Commit 06/10/2021_3

--- AcogeM/AcogeM_app/models.py
+++ AcogeM/AcogeM_app/models.py
@@ -13,23 +13,24 @@ class Protectora(models.Model):
 
 class Animal(models.Model):
 
-	LOAN_STATUS (
-		("p", "Perro"),
-		("g", "Gato"),
-		("h", "Huron"),
-		("c", "Conejo"),
-	)
-
-	tipo = models.CharField('Tipo', max_length=1, choices=LOAN_STATUS, default="p")
 	protectora = models.ForeignKey('Protectora', on_delete=models.CASCADE)
 	nom = models.CharField('Nombre', max_length=50)
 	descrip = models.CharField('Descripción', max_length=200)
 #	foto = models.ImageField('Imagen', blank=True, null=True, upload_to=localizacion_imagen)
 
+	TIPO_ANIMALES (
+	 	("p", "Perro"),
+	 	("g", "Gato"),
+	 	("h", "Huron"),
+		("c", "Conejo"),
+	)
+
+	tipo = models.CharField('Tipo', max_length=1, choices=TIPO_ANIMALES, blank=True, default="p")
+
 class User(models.Model):
 	nom = models.CharField('Nombre', max_length=30)
 	ape = models.CharField('Apellidos', max_length=50)
-	dni = forms.ESIdentityCardNumberField(only_nif=True)
-	tel = models.IntegerField('Teléfono', max_digits=9)
+	dni = models.CharField(max_length=9)
+	tel = models.IntegerField('Teléfono', blank=True, null=True)
 	exp = models.CharField('Experiencia con animales', max_length=200)
 
