1466845f89461db90ad8c81eeccf9cf7578f6d05
Israel Silva <escorpion45678@gmail.com>
2021-10-06 19:36:31 +0200

    Commit 06/10/2021_5

--- AcogeM/AcogeM_app/admin.py
+++ AcogeM/AcogeM_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Ciudad, Protectora, Animal, Perfil
+
+admin.site.register(Ciudad)
+admin.site.register(Protectora)
+admin.site.register(Animal)
+admin.site.register(Perfil)
