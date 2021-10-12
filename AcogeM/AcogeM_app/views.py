d9cebef899340f2e7d586fda81a67b83db5af8f6
Israel Silva <escorpion45678@gmail.com>
2021-10-12 18:50:19 +0200

    Commit P2
    Empezando las vistas

--- AcogeM/AcogeM_app/views.py
+++ AcogeM/AcogeM_app/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic import ListView
-from AcogeM_app.models import Ciudad
+from AcogeM_app.models import Ciudad, Protectora, Animal, Perfil
 
 class CiudadListView(ListView):
 	model = Ciudad
+
+class ProtectoraListView(ListView):
+	model = Protectora
+
+class AnimalListView(ListView):
+	model = Animal
+
+class PerfilListView(ListView):
+	model = Perfil
